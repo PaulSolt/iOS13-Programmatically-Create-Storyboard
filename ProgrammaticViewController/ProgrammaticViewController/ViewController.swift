c37e4252cb6ab47b51a1ac7a8886ef736e07c550
Paul Solt <PaulSolt@gmail.com>
2020-01-26 11:26:05 -0500

    Starting project with Storyboard

---
+++ ProgrammaticViewController/ProgrammaticViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticViewController
+//
+//  Created by Paul Solt on 1/26/20.
+//  Copyright © 2020 Paul Solt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
